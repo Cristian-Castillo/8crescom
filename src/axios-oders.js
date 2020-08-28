a481961ca5a38f4482b633ae8ad7a9cff177bd4e
Cristian C. Castillo <cris.c.castillo1@gmail.com>
2020-08-28 10:19:45 -0700

    Update axios-oders.js

--- src/axios-oders.js
+++ src/axios-oders.js
@@ -1,19 +1,6 @@
 import axios from 'axios';
 
-/* Request to store data in firebase data base 
-Copy and paste your firebase
 
-set rules... tab: copy and paste this into rules -
-
-
-{
-  "rules": {
-    ".read": "true",
-    ".write": "true"
-  }
-}
-
-*/
 
 /* Copy & paste firebase realtime data base link here */
 const instance = axios.create({
